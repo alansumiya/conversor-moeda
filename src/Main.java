121691943272587af22c64098d483ad54d6d6783
Alan Sumiya <alan.sumiya@gmail.com>
2025-05-05 10:44:11 -0300

    Criação do menu e record Moedas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       Scanner ler = new Scanner(System.in);
+       int opcao = 0;
+
+       while(opcao != 7){
+           System.out.println("""
+                   ##                               ##
+                   ##            Conversor          ##
+                   ##               de              ##
+                   ##             Moedas            ##
+                   ##                               ##
+                   Seja bem-vindo!
+                   
+                   1 - Dólar -->  Real
+                   2 - Real --> Dólar
+                   3 - Iene --> Real
+                   4 - Real --> Iene
+                   5 - Won --> Real
+                   6 - Real --> Won
+                   7 - Sair
+                   Escolha a opção desejada:
+                   """);
+       }
     }
 }
---
+++ src/Moedas.java
@@ -0,0 +1,2 @@
+public record Moedas(String usd, String brl, String  jpy, String kwr) {
+}
