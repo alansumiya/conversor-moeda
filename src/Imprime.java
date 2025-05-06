ce0352dee201168034b88cbd4b73e430baf85520
Alan Sumiya <alan.sumiya@gmail.com>
2025-05-06 09:21:29 -0300

    Criação da classe Imprime

---
+++ src/Imprime.java
@@ -0,0 +1,70 @@
+public class Imprime {
+
+    ConsultaMoeda consultaMoeda = new ConsultaMoeda();
+    ValorFinalConversao valorFinalConversao = new ValorFinalConversao();
+
+
+    public void ImprimeCotacao(String busca, int opcao){
+        Moeda novaMoeda = consultaMoeda.buscaMoeda(busca);
+        System.out.println(busca + ": " + novaMoeda.conversion_rates().get(busca));
+        if(opcao == 1 || opcao == 3 || opcao == 5){
+            String siglaMoedaConvertida = "BRL";
+            System.out.println(siglaMoedaConvertida + ": " + novaMoeda.conversion_rates()
+                    .get(siglaMoedaConvertida));
+        } else if (opcao == 2) {
+            String siglaMoedaConvertida = "USD";
+            System.out.println(siglaMoedaConvertida + ": " + novaMoeda.conversion_rates()
+                    .get(siglaMoedaConvertida));
+        } else if (opcao == 4) {
+            String siglaMoedaConvertida = "JPY";
+            System.out.println(siglaMoedaConvertida + ": " + novaMoeda.conversion_rates()
+                    .get(siglaMoedaConvertida));
+        } else if (opcao == 6) {
+            String siglaMoedaConvertida = "KRW";
+            System.out.println(siglaMoedaConvertida + ": " + novaMoeda.conversion_rates()
+                    .get(siglaMoedaConvertida));
+        }
+
+    }
+
+    public void ImprimeValorFinal(String busca, int opcao, double valorAConverter){
+        Moeda novaMoeda = consultaMoeda.buscaMoeda(busca);
+        if(opcao == 1) {
+            System.out.println(String.format("""
+                    Valor que deseja converter: US$ %.2f
+                    Valor final: R$ %.2f
+                    """, valorAConverter, valorFinalConversao
+                    .valorFinal(valorAConverter, novaMoeda.conversion_rates().get("BRL"))));
+        } else if (opcao == 2) {
+            System.out.println(String.format("""
+                            Valor que deseja converter: R$ %.2f
+                            Valor final: US$ %.2f
+                            """,valorAConverter, valorFinalConversao
+                    .valorFinal(valorAConverter,novaMoeda.conversion_rates().get("USD"))));
+
+        } else if (opcao == 3) {
+            System.out.println(String.format("""
+                            Valor que deseja converter: JP¥ %.2f
+                            Valor final: R$ %.2f
+                            """,valorAConverter, valorFinalConversao
+                            .valorFinal(valorAConverter,novaMoeda.conversion_rates().get("BRL"))));
+        } else if (opcao == 4) {
+            System.out.println(String.format("""
+                            Valor que deseja converter: R$ %.2f
+                            Valor final: JP¥ %.2f
+                            """,valorAConverter, valorFinalConversao
+                    .valorFinal(valorAConverter,novaMoeda.conversion_rates().get("JPY"))));
+        } else if (opcao == 5) {
+            System.out.println(String.format("""
+                            Valor que deseja converter: ₩ %.2f
+                            Valor final: R$ %.2f
+                            """,valorAConverter, valorFinalConversao
+                    .valorFinal(valorAConverter,novaMoeda.conversion_rates().get("BRL"))));
+        } else if (opcao == 6) {
+            System.out.println(String.format("""
+                            Valor que deseja converter: R$ %.2f
+                            Valor final: ₩ %.2f
+                            """,valorAConverter, valorFinalConversao
+                    .valorFinal(valorAConverter,novaMoeda.conversion_rates().get("KRW"))));        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
        Scanner ler = new Scanner(System.in);
-       ConsultaMoeda consultaMoeda = new ConsultaMoeda();
-       ValorFinalConversao valorFinalConversao = new ValorFinalConversao();
+//       ConsultaMoeda consultaMoeda = new ConsultaMoeda();
+//       ValorFinalConversao valorFinalConversao = new ValorFinalConversao();
+       Imprime imprime = new Imprime();
        int opcao = 0;
        var busca = "";
 
@@ -34,81 +35,45 @@ public class Main {
             switch (opcao){
                 case 1:
                     busca = "USD";
-                    Moeda novaMoeda = consultaMoeda.buscaMoeda(busca);
-                    System.out.println("BRL: " + novaMoeda.conversion_rates().get("BRL"));
-                    System.out.println("USD: " + novaMoeda.conversion_rates().get("USD"));
+                    imprime.ImprimeCotacao(busca, opcao);
                     System.out.print("Digite o valor que deseja converter: ");
                     valorAConverter = ler.nextDouble();
-                    System.out.println(String.format("""
-                            Valor que deseja converter: US$ %.2f
-                            Valor final: R$ %.2f
-                            """,valorAConverter, valorFinalConversao
-                            .valorFinal(valorAConverter,novaMoeda.conversion_rates().get("BRL"))));
+                    imprime.ImprimeValorFinal(busca, opcao, valorAConverter);
                     break;
                 case 2:
                     busca = "BRL";
-                    Moeda novaMoeda2 = consultaMoeda.buscaMoeda(busca);
-                    System.out.println("BRL: " + novaMoeda2.conversion_rates().get("BRL"));
-                    System.out.println("USD: " + novaMoeda2.conversion_rates().get("USD"));
+                    imprime.ImprimeCotacao(busca, opcao);
                     System.out.print("Digite o valor que deseja converter: ");
                     valorAConverter = ler.nextDouble();
-                    System.out.println(String.format("""
-                            Valor que deseja converter: R$ %.2f
-                            Valor final: US$ %.2f
-                            """,valorAConverter, valorFinalConversao
-                            .valorFinal(valorAConverter,novaMoeda2.conversion_rates().get("USD"))));
+                    imprime.ImprimeValorFinal(busca, opcao, valorAConverter);
                     break;
                 case 3:
                     busca = "JPY";
-                    Moeda novaMoeda3 = consultaMoeda.buscaMoeda(busca);
-                    System.out.println("JPY: " + novaMoeda3.conversion_rates().get("JPY"));
-                    System.out.println("BRL: " + novaMoeda3.conversion_rates().get("BRL"));
+                    imprime.ImprimeCotacao(busca, opcao);
                     System.out.print("Digite o valor que deseja converter: ");
                     valorAConverter = ler.nextDouble();
-                    System.out.println(String.format("""
-                            Valor que deseja converter: JP¥ %.2f
-                            Valor final: R$ %.2f
-                            """,valorAConverter, valorFinalConversao
-                            .valorFinal(valorAConverter,novaMoeda3.conversion_rates().get("BRL"))));
+                    imprime.ImprimeValorFinal(busca, opcao, valorAConverter);
                     break;
                 case 4:
                     busca = "BRL";
-                    Moeda novaMoeda4 = consultaMoeda.buscaMoeda(busca);
-                    System.out.println("BRL: " + novaMoeda4.conversion_rates().get("BRL"));
-                    System.out.println("JPY: " + novaMoeda4.conversion_rates().get("JPY"));
+                    imprime.ImprimeCotacao(busca, opcao);
                     System.out.print("Digite o valor que deseja converter: ");
                     valorAConverter = ler.nextDouble();
-                    System.out.println(String.format("""
-                            Valor que deseja converter: R$ %.2f
-                            Valor final: JP¥ %.2f
-                            """,valorAConverter, valorFinalConversao
-                            .valorFinal(valorAConverter,novaMoeda4.conversion_rates().get("JPY"))));
+                    imprime.ImprimeValorFinal(busca, opcao, valorAConverter);
                     break;
                 case 5:
                     busca = "KRW";
-                    Moeda novaMoeda5 = consultaMoeda.buscaMoeda(busca);
-                    System.out.println("KRW: " + novaMoeda5.conversion_rates().get("KRW"));
-                    System.out.println("BRL: " + novaMoeda5.conversion_rates().get("BRL"));
+                    imprime.ImprimeCotacao(busca, opcao);
                     System.out.print("Digite o valor que deseja converter: ");
                     valorAConverter = ler.nextDouble();
-                    System.out.println(String.format("""
-                            Valor que deseja converter: ₩ %.2f
-                            Valor final: R$ %.2f
-                            """,valorAConverter, valorFinalConversao
-                            .valorFinal(valorAConverter,novaMoeda5.conversion_rates().get("BRL"))));
+                    imprime.ImprimeValorFinal(busca, opcao, valorAConverter);
                     break;
                 case 6:
                     busca = "BRL";
-                    Moeda novaMoeda6 = consultaMoeda.buscaMoeda(busca);
-                    System.out.println("BRL: " + novaMoeda6.conversion_rates().get("BRL"));
-                    System.out.println("KRW: " + novaMoeda6.conversion_rates().get("KRW"));
+                    imprime.ImprimeCotacao(busca, opcao);
                     System.out.print("Digite o valor que deseja converter: ");
                     valorAConverter = ler.nextDouble();
-                    System.out.println(String.format("""
-                            Valor que deseja converter: R$ %.2f
-                            Valor final: ₩ %.2f
-                            """,valorAConverter, valorFinalConversao
-                            .valorFinal(valorAConverter,novaMoeda6.conversion_rates().get("KRW"))));
+                    imprime.ImprimeValorFinal(busca, opcao, valorAConverter);
                     break;
                 case 7:
                     break;
